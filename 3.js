d1c2d5d4e2b87ca3f8af87457a510cd4f5999e57
Daniella Korossy <koda5055@gmail.com>
2016-01-22 11:06:08 +0100

    add solution to 3.js

--- 3.js
+++ 3.js
@@ -8,11 +8,26 @@
 // 'Radius: 4, Circumference: 25.132741228718345, Area: 50.26548245743669'
 
 
+function Circle(radius) {
+  this.radius = radius;
+  this.getCircumference = function() {
+    this.circumference = this.radius*2*3.14;
+  };
+  this.getArea = function() {
+    this.area = this.radius *3.14*3.14
+  };
+  this.toString = toString;
+}
 
+var toString = function() {
+  this.getArea;
+  this.getCircumference;
+  console.log('Radius' + this.radius + ', Circumference: ' + this.circumference + ', Area: ' + this.area);
+}
 
+var kor1 = new Circle(4);
 
+kor1.getCircumference();
+kor1.getArea();
 
-
-
-
-
+kor1.toString();
