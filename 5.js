6b7bef4e3c443a1afcf1d7e5ad7362bbe45279c6
Daniella Korossy <koda5055@gmail.com>
2016-01-22 12:13:59 +0100

    add solution to 5.js without delete function

--- 5.js
+++ 5.js
@@ -2,16 +2,10 @@
 
 // Create a constructor called CarStore, that takes an array of cars as a parameter:
 // It should have an "addCar" method that adds a car object to it's list
-// It should have a "getSumPrice" method that returns the sum of all cars price 
+// It should have a "getSumPrice" method that returns the sum of all cars price
 // It should have a "getOldestCarType" method that returns the oldest car's type
 // It should have a "deleteCarByType" method that removes the cars from the inner list
 // that have the given type
-
-
-
-
-
-
 
 var cars = [
   {type: 'Dodge', price: 20000, year: 2012},
@@ -21,11 +15,56 @@ var cars = [
   {type: 'Ferrari', price: 40000, year: 2001}
 ];
 
+
+function CarStore(cars) {
+  this.cars = cars;
+  this.addCar = addCar;
+  this.getSumPrice = getSumPrice;
+  this.getOldestCarType = getOldestCarType;
+}
+
+function addCar(tipus, ar, ev) {
+	var newCar = {
+		type: tipus,
+		price: ar,
+		year: ev
+	};
+	this.cars.push(newCar);
+	return this.cars;
+}
+
+function getSumPrice() {
+  var sum = 0;
+  for (var i =0; i < this.cars.length; i++) {
+    sum += this.cars[i].price;
+  }
+  return sum
+}
+
+function getOldestCarType() {
+  var oldest = this.cars[0].year;
+  var oldestCar = this.cars[0].type;
+  for (var i = 0; i < this.cars.length; i++) {
+    if (this.cars[i].year < oldest) {
+      oldest = this.cars[i].year;
+      oldestCar = this.cars[i].type;
+    }
+  }
+  return oldestCar;
+}
+
+function deleteCarByType(tipus) {
+  for (var i = 0; i < this.cars.length < i++) {
+    
+  }
+}
+
+
 var carStore = new CarStore(cars);
 
-carStore.addCar('Smart', 18000, 2011);
-console.log(carStore.getSumPrice()); // 122000 
-console.log(carStore.getOldestCarType()); // 'Trabant'
-carStore.deleteCarByType('Ferrari');
-console.log(carStore.getSumPrice()); // 82000 
 
+carStore.addCar('Smart', 18000, 2011);
+console.log(carStore.getSumPrice()); // 122000
+console.log(carStore.getOldestCarType()); // 'Trabant'
+// carStore.deleteCarByType('Ferrari');
+console.log(carStore.getSumPrice()); // 82000
