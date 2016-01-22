8fe29f71fcc72ced870694249d43fdb19d5814cf
Daniella Korossy <koda5055@gmail.com>
2016-01-22 12:00:10 +0100

    solution to 4.js

--- 4.js
+++ 4.js
@@ -3,7 +3,16 @@
 // Create a function that counts all the letters in a string,
 // and returns an object that has the letters as keys and the counts as values.
 
-
+function countLetters(string) {
+  var result = {};
+  for (var i = 0; i < string.length; i++) {
+    if (result[string[i]] === undefined) {
+      result[string[i]] = 1;
+    }
+    result[string[i]]++;
+  }
+  return result;
+}
 
 
 
